4542a556bd4a1b680bfcaad6168fe4c0413f1814
DianeChoi14 <miseol914@gmail.com>
2024-07-09 16:39:46 +0900

    list all 메서드 추가

---
+++ src/main/java/com/miniproj/controller/hboard/HboardController.java
@@ -0,0 +1,15 @@
+package com.miniproj.controller.hboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // 아래의 클래스가 컨트롤러 객체임을 명시
+@RequestMapping("/hboard")
+public class HboardController 
+{
+	@RequestMapping("/listAll")
+	public void listAll()
+	{
+		// return "/hboard/listAll" // "/hboard/listAll.jsp"
+	}
+}
