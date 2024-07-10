deb35afe9d91064caf5f56a8b841235630085800
DianeChoi14 <miseol914@gmail.com>
2024-07-10 14:52:32 +0900

    주석문 수정

--- src/main/java/com/miniproj/controller/HomeController.java
+++ src/main/java/com/miniproj/controller/HomeController.java
@@ -6,22 +6,23 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
-@Controller
-public class HomeController {
-	
+import com.miniproj.service.hboard.HBoardService;
+
+@Controller // 아래의 클래스가 컨트롤러 객체임을 명시
+public class HomeController 
+{
+	// log를 남길 수 있도록 하는 객체
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private HBoardService service;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
