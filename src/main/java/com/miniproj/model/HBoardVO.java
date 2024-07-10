f4116191765a8fd019b4d9daad19188caa644a96
DianeChoi14 <miseol914@gmail.com>
2024-07-10 12:02:51 +0900

    feat : 계층형 테이블 Hboard VO객체 생성

---
+++ src/main/java/com/miniproj/model/HBoardVO.java
@@ -0,0 +1,29 @@
+package com.miniproj.model;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class HBoardVO
+{
+	private int boardNo;
+	private String title;
+	private String content;
+	private String writer;
+	private Timestamp postDate;
+	private int readCount;
+	private int ref ;
+	private int step ;
+	private int refOrder ;
+}
