76898d6e69d85bf09f493acd06fdbd22844098f1
DianeChoi14 <miseol914@gmail.com>
2024-07-11 16:53:03 +0900

    feat:SaveBoard 작성(미완)

--- src/main/java/com/miniproj/service/hboard/HBoardService.java
+++ src/main/java/com/miniproj/service/hboard/HBoardService.java
@@ -2,7 +2,14 @@ package com.miniproj.service.hboard;
 
 import java.util.List;
 
+import com.miniproj.model.HBoardDTO;
 import com.miniproj.model.HBoardVO;
+
+//// Service단에서 해야할 작업
+//1) Controller단에서 넘겨진 파라미터를 처리한 후(비즈니스 로직에 의해(트랜잭션처리를 통해))
+//2) DB작업이라면 DAO단 호출
+//3) DAO단에서 반환된 값을 Controller단으로 넘겨줌
+
 
 // 클래스를 구현하기 전 메서드의 집합
 public interface HBoardService 
@@ -12,6 +19,7 @@ public interface HBoardService
 	public List<HBoardVO> getAllBoard() throws Exception; // 라는 메서드를 조회한다(impl)~
 
 	// 게시판 글 작성
+	public boolean saveBoard(HBoardDTO newBoard) throws Exception;
 	
 	// 게시판 글 수정
 	
