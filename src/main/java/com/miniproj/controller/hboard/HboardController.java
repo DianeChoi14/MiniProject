6aa2f5887e32fd1438df4d0511ff7f3c1b460f0e
DianeChoi14 <miseol914@gmail.com>
2024-07-11 16:55:03 +0900

    feat:게시글 작성 메서드(미완)

--- src/main/java/com/miniproj/controller/hboard/HboardController.java
+++ src/main/java/com/miniproj/controller/hboard/HboardController.java
@@ -14,6 +14,14 @@ import com.miniproj.model.HBoardDTO;
 import com.miniproj.model.HBoardVO;
 import com.miniproj.service.hboard.HBoardService;
 
+//// 컨트롤러단에서 해야할 일 
+//1) uri맵핑(어떤 uri가 어떤방식(get/post)으로 호출되었을 때 어떤 메서드에 매핑시킬 것인가)
+//2) 있다면 view단에서 보내준 매개변수 수집
+//3) 데이터베이스에 대한 CRUD수행하기 위해 서비스단 매서드 호출
+//4) 서비스단에서 리턴한 값을 뷰단으로 바인딩, 뷰단 호출
+//5) 부가적으로 컨트롤러단은 Servlet에 의해 동작하는 모듈이기 때문에 HttpServletRequest, HttpServletResponse, HttpSession등의
+//	 Servlet객체들을 이용할 수 있다 > 이러한 객체들을 이용하여 구현할 기능이 있다면그 기능은 Controller단에서 구현한다
+
 @Controller // 아래의 클래스가 컨트롤러 객체임을 명시
 @RequestMapping("/hboard")
 public class HboardController 
@@ -62,9 +70,9 @@ public class HboardController
    
    // 게시글 저장버튼을 눌렀을 때 해당 게시글을 db에 저장하는 메서드
    @RequestMapping(value="/saveBoard", method=RequestMethod.POST) // get방식은 메서드방식 생략가능 
-   public void saveBoard(HBoardDTO boardDTO)
+   public void saveBoard(HBoardDTO boardDTO) throws Exception
    {
 	   System.out.println("이 게시글을 저장하자~!~~~"+boardDTO.toString());
-	   
+	   service.saveBoard(boardDTO);
    }
 }
