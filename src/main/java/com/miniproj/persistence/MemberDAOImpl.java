7db6858f55761210b40e70a8d0ad559bfac3a189
DianeChoi14 <miseol914@gmail.com>
2024-07-12 12:34:33 +0900

    feat : updateUserPoint 메서드 추가

---
+++ src/main/java/com/miniproj/persistence/MemberDAOImpl.java
@@ -0,0 +1,24 @@
+package com.miniproj.persistence;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+
+	@Autowired
+	private SqlSession ses;
+	
+	private static String NS = "com.miniproj.mappers.membermapper";
+	// static은 클래스가 로딩될 때 모든 메소드들이 공유한다, static이 없으면 객체가 만들어 져야 메모리에 올라감
+	
+	@Override
+	public int updateUserPoint(String userId) throws Exception {
+		
+		return ses.update(NS + ".updateUserPoint", userId);
+	}
+
+	
+}
+
