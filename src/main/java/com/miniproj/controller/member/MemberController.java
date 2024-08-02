4eeb0057814ddc5920c262976fc7852772b3309e
DianeChoi14 <miseol914@gmail.com>
2024-08-02 16:55:46 +0900

    feat : 메일 보내기, 인증코드비교, 인증코드 삭제 구현

--- src/main/java/com/miniproj/controller/member/MemberController.java
+++ src/main/java/com/miniproj/controller/member/MemberController.java
@@ -2,18 +2,25 @@ package com.miniproj.controller.member;
 
 import java.util.UUID;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.miniproj.model.MemberVO;
 import com.miniproj.model.MyResponseWithoutData;
 import com.miniproj.service.member.MemberService;
+import com.miniproj.util.SendMailService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -59,10 +66,43 @@ public class MemberController {
 		return result;
 	}
 	
-	@RequestMapping("/callSendMail")
-	public void sendMailAuthCode(@RequestParam("tmpUserEmail") String tmpUserEmail) {
+	@RequestMapping(value="/callSendMail")
+	public ResponseEntity<String> sendMailAuthCode(@RequestParam("tmpUserEmail") String tmpUserEmail, HttpSession session) {
 		String authCode = UUID.randomUUID().toString();
 		System.out.println(tmpUserEmail + "로" + authCode + "인증코드 보내자~");
+		String result = "";
 		// 이메일 전송을 완료한 뒤에 코드를 세션영역에 저장
+		try {
+			new SendMailService().sendMail(tmpUserEmail, authCode);
+			// 인증코드를 세션객체에 저장
+			session.setAttribute("authCode", authCode);
+			
+			result = "success";
+		} catch (MessagingException e) {
+			// 메일발송에 실패했을 경우
+			e.printStackTrace();
+			result = "fail";
+		}
+		return new ResponseEntity<String>(result, HttpStatus.OK);
 	}
+	@RequestMapping("/checkAuthCode")
+	public ResponseEntity<String> checkAuthCode(@RequestParam("tmpUserAuthCode")String tmpUserAuthCode, HttpSession session) {
+		System.out.println(tmpUserAuthCode + "코드 잘 확인했어요~ 이제 비교도 하세요~~~");
+		String result = "fail";
+		if(session.getAttribute("authCode")!=null) {
+			String sesAuthCode = (String)session.getAttribute("authCode");
+			if(tmpUserAuthCode.equals(sesAuthCode)) {
+				result = "success";
+			}
+		}
+		return new ResponseEntity<String>(result, HttpStatus.OK);
+	}
+	@RequestMapping("/clearAuthCode")
+	   public ResponseEntity<String> clearCode(HttpSession session) {
+	      if (session.getAttribute("authCode") != null) {
+	         session.removeAttribute("authCode");  // attribute 속성을 지운다...
+	      }
+	      
+	      return new ResponseEntity<String>("success", HttpStatus.OK);
+	   }
 }
