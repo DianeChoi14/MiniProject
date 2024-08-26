e86293f25d07f50022c7b65c11f30995de28288d
DianeChoi14 <miseol914@gmail.com>
2024-08-26 11:15:08 +0900

    feat : AOP 개념 학습

---
+++ src/main/java/com/miniproj/aop/ExampleAdvice.java
@@ -0,0 +1,27 @@
+package com.miniproj.aop;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component // 스프링컨테이너에게 객체를 만들어 관리하도록 설정하는 어노테이션
+@Aspect // 아래의 객체를 AOP객체로 사용할 것임을 명시하는 어노테이션
+public class ExampleAdvice {
+	// Advice : AOP로 구현할 객체를 명시
+	private static final Logger logger = LoggerFactory.getLogger(ExampleAdvice.class);
+	
+	// 인터셉터 - 특정 uri를 실행할 때 작동 됨, 컨트롤러단에 건다-서블릿에 의해 동작하므로..
+	// AOP - 특정틀래스-메서드를 실행할 때 작동 됨, 서비스단에 건다
+	@Before("execution(public * com.miniproj.service.hboard.HBoardServiceImpl.*(..))")
+	public void startAOP() {
+		System.out.println("============================== AOP 시작 =================================");
+	}
+	
+	@After("execution(public * com.miniproj.service.hboard.HBoardServiceImpl.*(..))")
+	public void endAOP() {
+		System.out.println("================================ AOP 끝 =================================");
+	}
+}
